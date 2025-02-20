add26bbb5e90815c3bab1e5a6884ffd1e741dddf
Your Name <you@example.com>
2025-02-20 14:30:12 +0100

    Added Occupancy grid node

--- install/arm_servos_pubs_subs/share/arm_servos_pubs_subs/package.sh
+++ install/arm_servos_pubs_subs/share/arm_servos_pubs_subs/package.sh
@@ -80,7 +80,6 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/arm_servos_pubs_subs/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/arm_servos_pubs_subs/hook/ament_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/arm_servos_pubs_subs/share/arm_servos_pubs_subs/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
