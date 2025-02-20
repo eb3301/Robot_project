cae7cd29caa3e6104d41dfdc468d43d5d5607030
Loke <loken@kth.se>
2025-02-20 11:27:49 +0100

    fixed recent merge conflicts

--- src/steering/steering/auto_control.py
+++ src/steering/steering/auto_control.py
@@ -20,14 +20,10 @@ class AutoControll(Node):
         self.cmd_vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
 
         self.pose_sub = self.create_subscription(PoseStamped, "/odom_pose",
-<<<<<<< HEAD
-                                                  self.pose_callback, 10)
-=======
                                                    self.pose_callback, 10)
         
         # self.goal_pose_pub = self.create_publisher(PoseStamped, "/goal_pose", 10)
         self.goal_marker_pub = self.create_publisher(Marker, "/goal_marker", 10)  # Publisher for marker
->>>>>>> Loke
 
         self.goal_pose = (0, 0)
         self.pose = Pose()
@@ -36,24 +32,11 @@ class AutoControll(Node):
 
     #Updates current pose
     def pose_callback(self, msg: PoseStamped):
-<<<<<<< HEAD
-        # latest_PoseStamped = msg.poses[-1] #PoseStamped
-        self.pose = msg.pose #Pose
-
-    
-    #Calculates path to destination
-    def calculate_path(self):
-        curr_x, curr_y = self.pose.position.x, self.pose.position.y
-        goal_x, goal_y = self.goal_pose[0], self.goal_pose[1]
-        print(f"x: {self.pose.position.x}, y: {self.pose.position.y}")
-        #2D stearing twist msg
-=======
         self.pose = msg.pose #Pose
 
         # self.pub_goal_pose()
         self.pub_goal_marker()
         #Create twist msg
->>>>>>> Loke
         twist_msg = Twist()
         #All 0 for 2D steering
         twist_msg.linear.y = 0.0
@@ -63,24 +46,9 @@ class AutoControll(Node):
         #Arbitrary velocity
         twist_msg.linear.x = 0.20
 
-<<<<<<< HEAD
-        #check if robot arrived at goal posititon
-        min_error = 0.1
-        dist = np.sqrt((goal_x - curr_x)**2 +
-                       (goal_y - curr_y)**2)
-        if dist < min_error:
-            self.arrived = True
-            twist_msg.linear.x = 0.0
-            twist_msg.angular.z = 0.0
-            return 
-        
-        #Compute path to destination
-
-=======
         #Calculate correct desired steering
         curr_x, curr_y = self.pose.position.x, self.pose.position.y
         goal_x, goal_y = self.goal_pose[0], self.goal_pose[1]
->>>>>>> Loke
         dx, dy = goal_x - curr_x, goal_y - curr_y
         heading  = self.compute_heading(self.pose.orientation)
 
@@ -139,13 +107,8 @@ class AutoControll(Node):
     
     
     def generate_point(self):
-<<<<<<< HEAD
-        x, y = (random.uniform(0, 20), random.uniform(0, 20))
-        # y = (random.uniform(0, 2), random.uniform(0, 2))
-=======
         x, y = (random.uniform(0, 2), random.uniform(0, 2))
         print(f"Moving to marker at:({x, y})")
->>>>>>> Loke
         return x, y
 
     # def pub_goal_pose(self):
@@ -192,23 +155,7 @@ def main():
     node.goal_pose = (x, y)
     _ = input("Press enter to start moving!")
     try:
-<<<<<<< HEAD
-        while True:
-            #Sample new destination point
-            if node.arrived:
-                node.arrived = False
-                #sample random point within x:[0, 2], y:[0, 2]
-                x, y = node.generate_point()
-                node.goal_pose = (x, y)
-                print(f"Sampled point: (x,y) = ({x,y})")
-            else: 
-                rclpy.spin_once(node)
-                node.calculate_path()
-                time.sleep(3) #Waiting 3s before calculating new path
-                
-=======
         rclpy.spin(node)
->>>>>>> Loke
     except KeyboardInterrupt:
         pass
 
