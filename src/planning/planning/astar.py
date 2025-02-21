614cc20f177a3c5a70a9b7a9bcf44dcb19960082
johanallberg <jallberg@kth.se>
2025-02-21 13:30:47 +0100

    Planning

--- src/planning/planning/astar.py
+++ src/planning/planning/astar.py
@@ -4,44 +4,100 @@ import rclpy
 import rclpy.logging
 import math
 from rclpy.node import Node
-from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid, Path
 from visualization_msgs.msg import Marker
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from tf_transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
+from tf_transformations import euler_from_quaternion, quaternion_from_euler
 
 class Planner(Node):
 
   def __init__(self):
-    super().__init__('Planner')
+    super().__init__('Initialize_Planner')
 
     # Subscribe to costmap
     self.costmap_sub = self.create_subscription(OccupancyGrid, '/map', self.map_callback, 10)
 
     # Subscribe to current pose
-    self.pose_sub = self.create_subscribtion(PoseWithCovarianceStamped, '/pose', self.pose_callback, 10)
+    self.pose_sub = self.create_subscription(PoseWithCovarianceStamped, '/pose', self.pose_callback, 10)
     
     # Subscribe to goal pose
     self.goal_sub = self.create_subscription(Marker, "/goal_marker", self.goal_callback, 10)
 
+    # Publish the path
+    self.path_pub = self.create_publisher(Path, "/path", 10)
+
+    # Prealocation
+    self.obsticales = []
+    self.resolution = 0
+
+    # Robot pose coordnates
+    self.x0 = 0
+    self.y0 = 0
+    self.theta0 = 0
+
+    # Target coordinates
+    self.xt = 0.5
+    self.yt = 0.5
+
   def map_callback(self, msg : OccupancyGrid):
-    obsticales = msg.data
-    resolution = msg.info.resolution
-    origo_x = msg.info.origin.position.x
-    origo_y = msg.info.origin.position.x
+    map_data = msg.data
+    self.resolution = msg.info.resolution
+    grid_size = msg.info.height
+    # origo_x = msg.info.origin.position.x
+    # origo_y = msg.info.origin.position.x
+
+    self.obsticales = self.find_obstacles(map_data, grid_size, self.resolution)
+
+    path, time = solution(self.x0, self.y0, self.theta0, self.xt, self.yt, self.obsticales)
+
+    message = Path()
+    message.header.stamp = self.get_clock().now().to_msg()
+    message.header.frame_id = 'map'
+    for i, pose in enumerate(path):
+      pose_msg = PoseStamped()
+      pose_msg.header.stamp = message.header.stamp
+      pose_msg.header.frame_id = message.header.frame_id
+      # Set the position and orientation (phi in your case)
+      pose_msg.pose.position.x = pose[0]
+      pose_msg.pose.position.y = pose[1]
+      # Convert the orientation from phi (heading) to a quaternion
+      quaternion = quaternion_from_euler(0, 0, pose[2])  # Use phi as the yaw angle
+      pose_msg.pose.orientation.x = quaternion[0]
+      pose_msg.pose.orientation.y = quaternion[1]
+      pose_msg.pose.orientation.z = quaternion[2]
+      pose_msg.pose.orientation.w = quaternion[3]
+      
+      message.poses.append(pose_msg)
+
+    self.path_pub.publish(message)
 
   def pose_callback(self, msg : PoseWithCovarianceStamped):
-    x0 = msg.pose.pose.position.x
-    y0 = msg.pose.pose.position.y
-    theta0 = self.compute_heading(msg.pose.pose.orientation)
+    self.x0 = msg.pose.pose.position.x
+    self.y0 = msg.pose.pose.position.y
+    self.theta0 = self.compute_heading(msg.pose.pose.orientation)
 
   def goal_callback(self, msg : Marker):
-    xt = msg.pose.position.x
-    yt = msg.pose.position.y
+    self.xt = msg.pose.position.x
+    self.yt = msg.pose.position.y
 
   def compute_heading(self, orientation):
         x, y, z, w = orientation.x, orientation.y, orientation.z, orientation.w
         _, _, yaw = euler_from_quaternion((x, y, z, w))
         return yaw
+  
+  def find_obstacles(self, map_data, grid_size=100, resolution=0.05):
+    obstacles = []
+    
+    for index, value in enumerate(map_data):
+        if value == 100:  # Check if the cell is an obstacle (value 100)
+            # Convert index to (x, y) coordinates
+            x = index // grid_size
+            y = index % grid_size
+            radius = resolution
+            obstacles.append((x, y, radius))
+    
+    return obstacles
+
 
 
 class Node(object):
@@ -59,7 +115,7 @@ class Node(object):
 
 
 def reached_target(x, y, xt, yt):
-  if math.sqrt(((x - xt)**2 + (y - yt)**2)) <= 0.5:
+  if math.sqrt(((x - xt)**2 + (y - yt)**2)) <= 0.5: # change distance to target
     return True
   return False
 
@@ -77,24 +133,11 @@ def step(x, y, theta, phi, dt=0.01):
   return xn, yn, thetan
 
 
-def step_collided_with_obsticale(obsticales, x, y, marign=0.2):
+def step_collided_with_obsticale(obsticales, x, y, marign=0.1):
   for obs in obsticales:
     if math.sqrt((x - obs[0])**2 + (y - obs[1])**2) <= (obs[2] + marign):
       return True
   return False
-
-
-def find_obstacles(map_data, grid_size=100):
-    obstacles = []
-    
-    for index, value in enumerate(map_data):
-        if value == 100:  # Check if the cell is an obstacle (value 100)
-            # Convert index to (x, y) coordinates
-            x = index // grid_size
-            y = index % grid_size
-            obstacles.append((x, y))
-    
-    return obstacles
 
 
 def grid(node, resolution=0.3):
@@ -108,9 +151,9 @@ def grid(node, resolution=0.3):
     return (x_key, y_key, theta_key)
     
 
-def get_new_nodes(current_node, open_set, closed_set, steps, xt, yt, obsticales): #, xlb, xub, ylb, yub):
+def get_new_nodes(current_node, open_set, closed_set, steps, xt, yt, obsticales):
   # Calculate different steering angles
-  for phi in [-math.pi/2, 0, math.pi/2]: # only grid coordinates
+  for phi in [-math.pi/4, 0, math.pi/4]: # only grid coordinates
     xn = current_node.x
     yn = current_node.y
     thetan = current_node.theta
@@ -139,15 +182,8 @@ def get_new_nodes(current_node, open_set, closed_set, steps, xt, yt, obsticales)
         new_node.phi = phi
         open_set[new_node_key] = new_node
 
-# x0 - robot x position
-# y0 - robot y position
-# xt - target x position
-# yt - target y position
-# obsticales - list of obsticales [x,y,radius]
 
-
-
-def solution(x0, y0, theta0, xt, yt, obsticales): #, xlb, xub, ylb, yub):
+def solution(x0, y0, theta0, xt, yt, obsticales):
   steps = 80
   start_node = Node(x0, y0, theta0) 
   start_node.h = math.sqrt(((start_node.x - xt)**2 + (start_node.y - yt)**2))
@@ -175,19 +211,18 @@ def solution(x0, y0, theta0, xt, yt, obsticales): #, xlb, xub, ylb, yub):
 
       while current_node:
           time.append(current_node.time)
-          path.append(current_node.phi)
+          path.append((current_node.x, current_node.y, current_node.phi))
           current_node = current_node.parent
       path.pop(-1)
       return path[::-1], time[::-1]
   
-    get_new_nodes(current_node, open_set, closed_set, steps, xt, yt, obsticales) #, xlb, xub, ylb, yub)
+    get_new_nodes(current_node, open_set, closed_set, steps, xt, yt, obsticales)
 
 
 
 
 def main():
     rclpy.init()
-    print('Hi from Planning.')
     node = Planner()
     try:
         rclpy.spin(node)
