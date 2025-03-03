7a77c4b9775ba5fbc4e4c57f78aae148f4b5bf87
Loke <loken@kth.se>
2025-03-03 20:58:21 +0100

    testing and small changes in urdf and service

--- src/arm_service/arm_service/arm_service.py
+++ src/arm_service/arm_service/arm_service.py
@@ -47,7 +47,7 @@ class MinimalService(Node):
         return (
             (np.all([-0.05 <= y <= 0.15, 0.0 <= x <= 0.18]) or 
             np.all([-0.15 <= y <= -0.05, -0.1 <= x <= 0.18]))
-            and (0.0 <= z <= 0.1)
+            and (0.0 <= z <= 0.2)
         )
 
 
@@ -56,13 +56,26 @@ class MinimalService(Node):
         urdf_file = os.path.join(os.path.dirname(__file__), "armpi_fpv.urdf")
         my_chain = ikpy.chain.Chain.from_urdf_file(urdf_file,active_links_mask=[True, True, True, True, True, True])
         
-        target_position = [ 0.1, 0, 0.05]
-        if not self.pos_ok_check(target_position):
-            self.get_logger().info("Position not within limits")
-            response.success = False # should add something like reason for fail maybe
-            return response
+        target_position = [ 0.00, 0.15, 0.04]
+        # if not self.pos_ok_check(target_position):
+        #     self.get_logger().info("Position not within limits")
+        #     response.success = False # should add something like reason for fail maybe
+        #     return response
 
         target_orientation = [0, 0, 0]
+
+        data_sets = [[12000,12000,12000,12000,12000,12000,2000,2000,2000,2000,2000,2000],
+                    [2000,12000,3000,12000,4000,12000,2000,2000,2000,2000,2000,2000],
+                    [11000,12000,10000,15000,4000,12000,2000,2000,2000,2000,2000,2000],
+                    [2000,12000,8000,16000,10000,12000,2000,2000,2000,2000,2000,2000]]
+        test_set = []
+        i=0
+        for val in data_sets[1]:
+            if i<6:
+                test_set.append((val - 12000)/100)
+            else: 
+                break
+            i += 1
 
         ik = my_chain.inverse_kinematics(target_position, target_orientation, orientation_mode="Y")
         ik_deg = list(map(lambda r:math.degrees(r),ik.tolist()))
@@ -72,37 +85,42 @@ class MinimalService(Node):
         for ang in ik_deg:
             new_ang = round(ang*100) + 12000
             computed_data_set.append(new_ang)
-        print("comp ik: "+ str(computed_data_set))
         time_data_set = [2000,2000,2000,2000,2000,2000]
         computed_data_set = np.concatenate((computed_data_set,time_data_set))
-        print(" send: " + str(computed_data_set))
         self.get_logger().info("Computed data set is: " + str(computed_data_set))
 
         computed_position = my_chain.forward_kinematics(ik)
+        computed_position2 = my_chain.forward_kinematics(test_set)
+
         print("Computed position: %s, original position : %s" % (computed_position[:3, 3], target_position))
         print("Computed position (readable) : %s" % [ '%.2f' % elem for elem in computed_position[:3, 3] ])
 
+        print("Computed top position: %s, original position : %s" % (computed_position2[:3, 3], target_position))
+        print("Computed top position (readable) : %s" % [ '%.2f' % elem for elem in computed_position2[:3, 3] ])
 
-        data_sets = [[12000,12000,12000,12000,12000,12000,2000,2000,2000,2000,2000,2000],
-                    [2000,12000,3000,12000,4000,12000,2000,2000,2000,2000,2000,2000],
-                    [11000,12000,10000,15000,4000,12000,2000,2000,2000,2000,2000,2000],
-                    [2000,12000,8000,16000,10000,12000,2000,2000,2000,2000,2000,2000]]
+
+        
         msg = Int16MultiArray()
-        msg.data = data_sets[int(request.xy[0]-1)]
+        
 
         if request.xy[0] == 1: # this is command from client
             self.get_logger().info('moving arm to top')
+            msg.data = data_sets[int(request.xy[0]-1)]
             self.publisher.publish(msg)
         elif request.xy[0] == 2:
             self.get_logger().info('moving arm to look')
+            msg.data = data_sets[int(request.xy[0]-1)]
             self.publisher.publish(msg)
         elif request.xy[0] == 3: 
             self.get_logger().info('moving arm to grab')
+            msg.data = data_sets[int(request.xy[0]-1)]
             self.publisher.publish(msg)
         elif request.xy[0] == 4: 
             self.get_logger().info('moving arm to drop')
+            msg.data = data_sets[int(request.xy[0]-1)]
             self.publisher.publish(msg)
         elif request.xy[0] == 5:
+            self.get_logger().info('moving arm to set point')
             msg.data = computed_data_set
             self.publisher.publish(msg)
         
